953d4a80c45407f341636d75dbbeceacfde97603
Deepak P G <deepakpg121@gmail.com>
2024-09-25 22:23:45 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace amartech.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
