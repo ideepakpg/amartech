63869649fdcdac8e30c132d854c13f0c4e6ba036
Deepak P G <deepakpg121@gmail.com>
2024-09-28 22:18:29 +0530

    Create Home controller and view

---
+++ Controllers/Admin/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace amartech.Controllers.Admin
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
