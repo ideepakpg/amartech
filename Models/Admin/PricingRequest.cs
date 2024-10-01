041bdf2bc3d01c5442fc237ac8e6a7e260d1c998
Deepak P G <deepakpg121@gmail.com>
2024-10-01 21:09:03 +0530

    Create PricingRequest model

---
+++ Models/Admin/PricingRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace amartech.Models.Admin
+{
+    public class PricingRequest
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Mobile { get; set; }
+        public string? Service { get; set; }
+        public string? SpecialNote { get; set; }
+        public bool IsActive { get; set; } = true;
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
