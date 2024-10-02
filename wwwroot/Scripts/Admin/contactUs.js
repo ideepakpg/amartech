59b46644af00e7cba29da24f4107d1a832bd7460
Deepak P G <deepakpg121@gmail.com>
2024-10-02 13:41:43 +0530

    Implement JS function for form submission with validation

---
+++ wwwroot/Scripts/Admin/contactUs.js
@@ -0,0 +1,48 @@
+﻿async function submitContactUs() {
+    debugger
+    const requestData = {
+        Name: document.getElementById('name').value,
+        Email: document.getElementById('email').value,
+        Subject: document.getElementById('subject').value,
+        Message: document.getElementById('message').value,
+    };
+
+    const { Name, Email, Subject, Message } = requestData;
+    if (!Name || !Email || !Subject || !Message) {
+        alert('Please fill in all fields before submitting');
+        return;
+    }
+
+    if (!/^[a-zA-Z\s]+$/.test(Name)) {
+        alert('Please enter a valid name');
+        return;
+    }
+
+    if (!/^\S+@\S+\.\S+$/.test(Email)) {
+        alert('Please enter a valid email address');
+        return;
+    }
+
+    try {
+        const response = await fetch('/Contact/SubmitContactUs', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(requestData),
+        });
+
+        if (response.ok) {
+            alert('Your message has been sent successfully!');
+            document.getElementById('name').value = '';
+            document.getElementById('email').value = '';
+            document.getElementById('subject').value = '';
+            document.getElementById('message').value = '';
+        } else {
+            alert('An error occurred while sending your message.');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('An unexpected error occurred.');
+    }
+}
