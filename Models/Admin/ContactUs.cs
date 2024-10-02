7a79ae161edbdd3df67f0d19c116a6fd03b8b9cb
Deepak P G <deepakpg121@gmail.com>
2024-10-02 13:43:10 +0530

    Create ContactUs model

---
+++ Models/Admin/ContactUs.cs
@@ -0,0 +1,15 @@
+﻿namespace amartech.Models.Admin
+{
+    public class ContactUs
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Subject { get; set; }
+        public string? Message { get; set; }
+        public bool IsActive { get; set; } = true;
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
